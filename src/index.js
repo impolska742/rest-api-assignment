d38fd853e3728bdb3da90fb897e1e8a06686a7a7
impolska742 <vaibhav19bhardwaj@gmail.com>
2022-09-06 12:56:53 +0530

    Added login state

--- src/index.js
+++ src/index.js
@@ -5,10 +5,23 @@ import "./bootstrap.min.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <App />
+    <ToastContainer
+      position="bottom-center"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+    />
   </Provider>
 );
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,24 +1,53 @@
+import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { toast } from "react-toastify";
 
 const Login = () => {
+  const [userId, setUserId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = (e) => {
+    e.preventDefault();
+    if (password !== "User@123") {
+      toast.error("Incorrect UserId or Password !", {
+        position: "bottom-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    } else {
+      localStorage.setItem("userInfo", {
+        userId,
+      });
+    }
+  };
   return (
-    <Form>
-      <Form.Group
-        style={{ marginTop: "40px" }}
-        className="mb-3"
-        controlId="formBasicEmail"
-      >
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+    <Form onSubmit={login}>
+      <Form.Group style={{ marginTop: "40px" }} className="mb-3">
+        <Form.Label>User ID</Form.Label>
+        <Form.Control
+          value={userId}
+          onChange={(e) => setUserId(e.target.value)}
+          type="number"
+          placeholder="Enter user id"
+        />
         <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
+          We'll never share your user id with anyone else.
         </Form.Text>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="Password"
+        />
       </Form.Group>
 
       <Button variant="dark" type="submit">
